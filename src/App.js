d33cc944bf561183706de881b2f73160ac07778f
Kaitlyn McGoldrick <kaitlynmcgoldrick@Kaitlyns-MacBook-Pro.local>
2018-03-10 15:39:42 -0500

    Add search functionality with Form Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Recipe } from './Components/recipe';
+import Form from './Components/form';
+
 
 const APP_ID = '98b85fc1'; // your app id here, provided to you when you sign up
 const APP_KEY = '2c5003e74a8b2a0bf2bba59d0a7b31f4'; // your app key here, provided to you when you sign up
@@ -21,12 +23,24 @@ class App extends Component {
   }
 
   componentDidMount()  {
-    fetch(`${BASE_ENDPOINT}&q=cake`)
+
+  }
+
+  handleFormSubmit = (item) => {
+    this.fetchRecipes(item);
+  }
+
+  fetchRecipes = (item) => {
+    fetch(`${BASE_ENDPOINT}&q=${item}`)
     .then(res => res.json())
     .then(res => {
       res.hits.length === 0 
-      ? this.setState({ error: "No recipes found 😞" })
-      : this.setState({ recipeList:  [...res.hits]  })
+      ? this.setState({ 
+        error: "No recipes found 😞",
+        recipeList: [] })
+      : this.setState({ 
+        error: '',
+        recipeList:  [...res.hits]  })
     })
     .catch(err => {
       this.setState({ error: err.message });
@@ -36,11 +50,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="error-text">
-          {this.state.error}
-        </div>
         <div className="recipe-list">
+          <Form handleFormSubmit={this.handleFormSubmit}/>
           <Recipe recipeList={this.state.recipeList} />
+          <div className="error-text">
+            {this.state.error}
+          </div>
         </div>
       </div>
     );
